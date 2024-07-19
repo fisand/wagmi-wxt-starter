a76f23b9dfa0de36282325d28dda196aa30d3aec
zouhang <zouhangsweet@gmail.com>
2024-07-19 14:50:12 +0800

    chore: refactor folders

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,34 +1,52 @@
-import react from '@vitejs/plugin-react'
-import UnoCSS from 'unocss/vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import nodePolyfills from 'vite-plugin-node-stdlib-browser'
-import { defineConfig } from 'wxt'
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import UnoCSS from "unocss/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import { FileSystemIconLoader } from "unplugin-icons/loaders";
+import Icons from "unplugin-icons/vite";
+import nodePolyfills from "vite-plugin-node-stdlib-browser";
+import Pages from "vite-plugin-pages";
+import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  srcDir: "src",
   vite: () => ({
     plugins: [
       react(),
       UnoCSS(),
       nodePolyfills(),
       AutoImport({
-        imports: ['react'],
-        dts: './auto-imports.d.ts',
+        imports: ["react"],
+        dts: "./auto-imports.d.ts",
+        dirs: ["./src/components/ui"],
+      }),
+      Pages({
+        dirs: [{ dir: "src/entrypoints/sidepanel/pages", baseRoute: "" }],
+        exclude: ["**/[A-Z]*.tsx"],
+        importMode: "sync",
+      }),
+      Icons({
+        compiler: "jsx",
+        jsx: "react",
+        customCollections: {
+          "op-icons": FileSystemIconLoader(
+            `${resolve(__dirname, "src/assets/icons")}/`,
+            (svg) => svg.replace(/^<svg /, '<svg fill="currentColor" '),
+          ),
+        },
       }),
     ],
-    optimizeDeps: {
-      include: ['extension-port-stream'],
-    },
   }),
   manifest: {
-    permissions: ['storage', 'sidePanel'],
+    permissions: ["storage", "sidePanel"],
     action: {
-      default_title: 'Click to open oracle panel',
+      default_title: "Click to open oracle panel",
     },
     web_accessible_resources: [
       {
-        resources: ['fonts/*.ttf'],
-        matches: ['*://*/*'],
+        resources: ["fonts/*.ttf"],
+        matches: ["*://*/*"],
       },
     ],
   },
-})
+});
